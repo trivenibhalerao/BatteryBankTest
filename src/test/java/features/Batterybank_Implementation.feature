5a3840c4a80f3720957b16c468d371e1aefd5653
LENOVO <neerajbhattneeraj@gmail.com>
2019-12-27 14:54:58 +0530

    bankbatteryTest

--- src/test/java/features/Batterybank_Implementation.feature
+++ src/test/java/features/Batterybank_Implementation.feature
@@ -1,6 +1,12 @@
 Feature: Verify API Status code
 
-Scenario: Verify API Status code of given API 
-    Given I make a "GET" call with URI "http://dummy.restapiexample.com/api/v1/employees" with endpoint "/1" is provided
-    Then api should return "404" status code
+Scenario: Verify API status code of GET API call 
+Given I make a GET call with URI "http://dummy.restapiexample.com/api/v1/employees" with endpoint "/1" is provided
+Then get  api should return "404" status code
+
+Scenario: Verify API status code for POST API call 
+Given I make a POST call with URI "http://dummy.restapiexample.com/api/v1" with endpoint "/create" 
+Then provided following details Name : "Hello" Salary : "1232123" Age : "12" with OTP : " " 
+Then post API should return "200" status code 
+
     
--- src/test/java/stepDefinitions/stepdefs.java
+++ src/test/java/stepDefinitions/stepdefs.java
@@ -1,6 +1,19 @@
 package stepDefinitions;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import com.google.common.net.HttpHeaders;
+
 import io.restassured.response.Response;
 import junit.framework.Assert;
 import cucumber.api.PendingException;
@@ -11,13 +24,14 @@ import cucumber.api.java.en.When;
 
 public class stepdefs {
 	public static String URI;
+	public static String jsonBody;
 
-	@Given("^I make a \"([^\"]*)\" call with URI \"([^\"]*)\" with endpoint \"([^\"]*)\" is provided$")
-	public void i_make_a_call_with_URI_with_endpoint_is_provided(String arg1, String arg2, String arg3) throws Throwable {
-		URI=arg2.concat(arg3);
+	@Given("^I make a GET call with URI \"([^\"]*)\" with endpoint \"([^\"]*)\" is provided$")
+	public void i_make_a_call_with_URI_with_endpoint_is_provided( String arg1, String arg2) throws Throwable {
+		URI=arg1.concat(arg2);
 	    System.out.println("URI you entered to test is :"+URI);
 	}
-	@Then("^api should return \"([^\"]*)\" status code$")
+	@Then("^get  api should return \"([^\"]*)\" status code$")
 	public void api_should_return_status_code(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("URI with endpoint "+URI);
@@ -28,5 +42,31 @@ public class stepdefs {
 	    Assert.assertEquals(InputStatusCode, response.statusCode());
 	}
 
+	@Given("^I make a POST call with URI \"([^\"]*)\" with endpoint \"([^\"]*)\"$")
+	public void i_make_a_POST_call_with_URI_with_endpoint(String arg1, String arg2) throws Throwable {
+		URI=arg1.concat(arg2);
+	    System.out.println("URI you entered to test is :"+URI);
+	}
 
+	@Then("^provided following details Name : \"([^\"]*)\" Salary : \"([^\"]*)\" Age : \"([^\"]*)\" with OTP : \"([^\"]*)\"$")
+	public void provided_following_details_Name_Phoneno_EmailID_with_OTP(String arg1, String arg2, String arg3, String arg4) throws Throwable {
+	  jsonBody="{\"name\":\""+arg1+"\",\"salary\":\""+arg2+"\",\"age\":\""+arg3+"\"}";
+	  System.out.println("\n\n" + jsonBody);
+	}
+	@Then("^post API should return \"([^\"]*)\" status code$")
+	public void post_API_should_return_status_code(String arg1) throws Throwable {
+		 HttpPost request = new HttpPost(URI);
+		  StringEntity entity = new StringEntity(jsonBody);
+		  request.addHeader("content-type", "application/json");
+		  request.setEntity(entity);
+		 CloseableHttpClient httpClient = HttpClients.createDefault();
+		  RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.DEFAULT).build();
+		  RequestConfig localConfig = RequestConfig.copy(globalConfig).setCookieSpec(CookieSpecs.STANDARD).build();
+		  request.setConfig(localConfig);
+		  HttpResponse response = httpClient.execute(request);
+		  System.out.println("Status code is " +response.getStatusLine().getStatusCode());
+		  assertEquals(200, response.getStatusLine().getStatusCode());
+	}
+
+	
 }
